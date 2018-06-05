019751298a2bc21b5b7e6091ef792e06ea93fc3c
Unknown <yanwenliqjl@gmail.com>
2018-06-05 10:57:56 +0200

    change

--- TP1/ex1_SalaireGestion/Salaire.java
+++
@@ -1,7 +0,0 @@
-package ex1_SalaireGestion;
-
-public interface Salaire {
-
-	public double getSalaire();
-	public void setInfoSalaire();//参数？
-}
--- TP5/ex1/Component.java
+++
@@ -1,9 +0,0 @@
-package ex1;
-
-public interface Component {
-
-	//Ce sont des methodes common entre des figures (composites) et ses filles (simples)
-	public void tranlater();
-	public void dessiner();
-	
-}
